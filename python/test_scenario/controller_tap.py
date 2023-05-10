8b8d0f8ce15b9648837ad0382fbbca49f5605083
olge <olge@elektro.dtu.dk>
2023-05-10 10:31:32 +0200

    Changing file structure of the python code with one subfolder per scenario.

---
+++ python/test_scenario/controller_tap.py
@@ -0,0 +1,62 @@
+import sys
+from pythonfmu import Fmi2Causality, Fmi2Slave, Real, Integer
+
+class controller_tap(Fmi2Slave):
+    """ 
+        Class to model a basic voltage controller to alter 
+        transfomer tap position.
+    
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        # initalize inputs and output
+        self.tap = 0
+        self.u3 = 1.0
+        self.u4 = 1.0
+        self.v_up = 1.0
+        self.v_low = 1.0
+        # register as inputs/output as per FMI standard
+        self.register_variable(Real("u3", causality=Fmi2Causality.input))
+        # self.register_variable(Real("u4", causality=Fmi2Causality.input))
+        self.register_variable(Real("v_up", causality=Fmi2Causality.input))
+        self.register_variable(Real("v_low", causality=Fmi2Causality.input))
+        self.register_variable(Integer("tap", causality=Fmi2Causality.output))
+        print("Controller co-simulation: INIT DONE")
+    
+    def do_step(self, current_time, step_size):
+        """ Function to update state of controller and change tap position if required.
+        Parameters
+        ----------
+        comm_point: float
+        current time step in co-simulation
+        comm_step_size: float
+        step size of co-simulation
+        """
+        # u3 = self.u3
+        # u4 = self.u4
+        # v_low = self.v_low
+        # v_high = self.v_up
+        # u_min = min(u3, u4)
+        # u_max = max(u3, u4)
+        # if (u_max > v_high):
+        #     self.tap += 1
+        # if (u_min < v_low):
+        #     self.tap -= 1
+        # if(self.tap > 2):
+        #     self.tap = 2
+        # elif(self.tap < -2):
+        #     self.tap = -2
+        u3 = self.u3
+        v_low = self.v_low
+        v_high = self.v_up
+        
+        if (u3 > v_high):
+            self.tap += 1
+        if (u3 < v_low):
+            self.tap -= 1
+        if(self.tap > 2):
+            self.tap = 2
+        elif(self.tap < -2):
+            self.tap = -2
+        return True
---
+++ python/test_scenario/lv_grid_pp.py
@@ -0,0 +1,40 @@
+import pandapower as pp
+import os
+
+net = pp.create_empty_network()
+    
+def create_network():
+    """create busbars as per TC-3 powerfactory model from ERIGrid 1. 
+    Network consists of one HV bus, three LV buses, an OLTC, and two loads
+    """
+    bus1 = pp.create_bus(net, name = "HV_Bus", vn_kv = 20, type = "b")
+    bus2 = pp.create_bus(net, name = "LV_Bus2", vn_kv = 0.4, type = "b")
+    bus3 = pp.create_bus(net, name = "LV_Bus3", vn_kv = 0.4, type = "b")
+    bus4 = pp.create_bus(net, name = "LV_Bus4", vn_kv = 0.4, type = "b")
+    # external grid connection
+    pp.create_ext_grid(net, bus1, vm_pu=1.00, va_degree=0.0) 
+    # connect transformer between bus 1 and bus 2
+    trafo1 = pp.create_transformer(net, bus1, bus2, name="OLTC", std_type="0.25 MVA 20/0.4 kV")
+    # connect bus 2-3 and bus 2-4 via lines
+    line1 = pp.create_line(net, bus2, bus3, length_km=2, std_type="NAYY 4x50 SE", name="Line-2-3")
+    line2 = pp.create_line(net, bus2, bus4, length_km=3, std_type="NAYY 4x50 SE", name="Line-2-4")
+    # create loads
+    pp.create_load(net, bus3, p_mw=3e-3, q_mvar=0.96e-3, name="load3")
+    pp.create_load(net, bus4, p_mw=3e-3, q_mvar=0.96e-3, name="load4")
+    return net
+
+def run_pf():
+    pp.runpp(net)
+    print(net.res_bus)
+    
+def save_nw():
+    # path to store pandapower network as'pickle' file
+    pandapower_net_file = os.path.join(os.getcwd(), 'erigrid_lv_nw.p') 
+    # load pickle file as FMU and use as pandapower network
+    pp.to_pickle(net, pandapower_net_file)
+    
+if __name__ == '__main__':
+    net = create_network()
+    run_pf()
+    save_nw()
+
