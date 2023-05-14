b17b700b36c56d6ef04d2a0cacddd1a1a5d49870
olge <olge@elektro.dtu.dk>
2023-05-14 23:58:56 +0200

    New run script for JRA1.2 case. Adding pandapower network generation to build script.

--- build_python_fmus.sh
+++ build_python_fmus.sh
@@ -1,5 +1,13 @@
 cd python/test_scenario
+python lv_grid_pp.py
 pythonfmu build -f lv_grid_fmu.py
 pythonfmu build -f controller_tap.py
 cd ../..
 mv python/test_scenario/*fmu fmus/
+
+cd python/jra1.2_scenario
+python lv_grid_jra12_pp.py
+pythonfmu build -f lv_grid_jra12_fmu.py
+pythonfmu build -f controller_voltage.py
+cd ../..
+mv python/jra1.2_scenario/*fmu fmus/
---
+++ run_jra12_scenario.sh
@@ -0,0 +1,2 @@
+cd python/jra1.2_scenario
+python scenario_jra12_collector.py
